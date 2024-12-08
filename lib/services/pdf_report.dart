365ec65f533879e63e6e0e23f43339e0c1b836d3
Nhaniso Undenge <nhanitech@gmail.com>
2024-12-08 12:09:59 +0200

    feat: more variables for all models calculations

--- lib/services/pdf_report.dart
+++ lib/services/pdf_report.dart
@@ -1,6 +1,6 @@
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:printing/printing.dart';
+import 'package:printing/printing.dart' show Printing;
 
 class PdfReport {
   Future<void> generateReport(String insuranceType, double premium) async {
